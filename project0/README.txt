fd7865df430ad2102e434d75abf419dfc2da719f
Justin Kleiber <jkleiber8@gmail.com>
2018-09-08 00:54:47 -0500

    Adds comments to the code and tests some more

---
+++ project0/README.txt
@@ -0,0 +1,31 @@
+Project 0
+
+Name: Justin Kleiber
+
+Email address: jkleiber@ou.edu | jkleiber8@gmail.com
+
+Date: 09/07/2018
+
+Description
+My approach to solve this problem was to read input from stdin using the 
+getchar() function until the input stream ended. I detect the end of stdin with 
+the feof() function as I found that comparing getchar to the EOF macro is 
+insufficient (EOF can be triggered by errors, as well as end-of-file). As 
+characters are read in, my program keeps track of the number of appearances of 
+each character in an array. After the stdin "file" reaches the end, I loop 
+through elements in the array based on the boundary characters provided by the 
+user and output the number of times they occured in the file while printing out 
+the corresponding number of # characters.
+To handle erroneous input, my program exits with no output if an incorrect number 
+of program arguments are passed in.
+
+References
+I used https://www.geeksforgeeks.org/eof-and-feof-in-c/ to learn more about 
+feof() and how getchar() handles errors and EOF.
+
+I also used http://www.cplusplus.com/reference/cstdio/printf/ to learn how to
+do the 4 character right-flush output formatting.
+
+I've used C on Linux a lot for work, so most of this was pretty familiar. I 
+did need to look at the man pages for memset to remember the ordering of the 
+arguments, and I also used the man pages to learn more about getchar().
--- project0/project0.c
+++ project0/project0.c
@@ -3,20 +3,23 @@
 #include <string.h>
 
 #define CLOSING_CURLY_BRACE 0x7d
-#define NUM_VALID_CHARS 95
+#define NUM_VALID_CHARS 256
 #define SPACE 0x20
 #define WIDTH 4
 
 /*
- *
+ * Adds a character to the occurences array if it is in bounds.
+ * 
+ * @param c: character to add to the array
+ * @param *occurences: integer array of the number of times a character is seen
  */
-void add_char(char c, int* occurences)
+void add_char(unsigned char c, int* occurences)
 {
     /* Declare variable */
-    int index; //Index in occurences array to increment
+    int index; //index in occurences array to increment
 
     //Find the index in the array based on the offset from the SPACE character
-    index = (int)(c - SPACE);
+    index = (int)(c - 0);
 
     //If the character is valid, increment the occurences counter
     if(index >= 0
@@ -27,12 +30,14 @@ void add_char(char c, int* occurences)
 }
 
 /*
- *
+ * This function prints N # characters.
+ * 
+ * @param num_hashtags: number of # characters to print (N)
  */
 void print_hashtags(int num_hashtags)
 {
     /* Declare variable */
-    int i;  //Loop iteration
+    int i;  //loop iteration
 
     //Print the number of hashtags requested
     for(i = 0; i < num_hashtags; ++i)
@@ -42,18 +47,21 @@ void print_hashtags(int num_hashtags)
 }
 
 /*
- *
+ * The main function. Sets up the program, reads the input and formats the output.
+ * 
+ * @param argc: the number of program arguments passed from the terminal
+ * @param **argv: the program arguments passed from the terminal
  */
 int main(int argc, char** argv)
 {
     /* Declare variables */
-    char c;                                 //iteration variable for looping through characters
-    char cur_char;                          //Current character read from 
-    int  index;                             //index of character in the occurences array
-    char lower_char;                        //Lowest character in ASCII table to check for in file
-    int  num_occurences[NUM_VALID_CHARS];   //number of occurences of each character in the file
-    char upper_char;                        //Highest character in ASCII table to check for in file
-    
+    unsigned char c;                                //iteration variable for looping through characters
+    unsigned char cur_char;                         //current character read from 
+    int           index;                            //index of character in the occurences array
+    unsigned char lower_char;                       //lowest character in ASCII table to check for in file
+    int           num_occurences[NUM_VALID_CHARS];  //number of occurences of each character in the file
+    unsigned char upper_char;                       //highest character in ASCII table to check for in file
+
     /* Initialize variables */
     upper_char = SPACE;                 //SPACE character is default
     lower_char = CLOSING_CURLY_BRACE;   //'}' is the default
@@ -79,38 +87,52 @@ int main(int argc, char** argv)
         exit(-1);
     }
 
-    //Prepare to read file
-    cur_char = getchar();
-
-    int i = 1;
-
-    //Read files from stdin (piped in from console)
-    while(!feof(stdin))
+    //Don't bother reading in the file unless the upper char is equal or lesser than lower char.
+    //Upper char needs to be greater than 0 to be on the ASCII table
+    if(upper_char <= lower_char
+    && upper_char >= 0)
     {
-        if(cur_char != EOF)
-        {
-            //Check to see if the character falls into a valid range in the tracking array
-            add_char(cur_char, num_occurences);
-        }
-
-        //Read next character
+        //Prepare to read file
         cur_char = getchar();
-        i++;
-    }
 
+        //Read files from stdin (piped in from console)
+        while(!feof(stdin))
+        {
+            if(cur_char != EOF)
+            {
+                //Check to see if the character falls into a valid range in the tracking array
+                add_char(cur_char, num_occurences);
+            }
+
+            //Read next character
+            cur_char = getchar();
+        }
+    }
 
     //Print header information
     fprintf(stdout, "Range: %c-%c\n", upper_char, lower_char);
 
-    //Loop through the array and print output
-    for(c = upper_char; c <= lower_char; ++c)
+    //Only perform additional output if the upper character is equal to or comes after the lower character.
+    //Also, the upper char should be at least 0 to avoid out of bounds errors
+    if(upper_char <= lower_char
+    && upper_char >= 0)
     {
-        index = c - SPACE;
+        //Loop through the array and print output
+        for(c = upper_char; c <= lower_char; ++c)
+        {
+            //Convert the character to a numeric index
+            index = (int)(c - 0);
 
-        fprintf(stdout, "%c: %*d ", c, WIDTH, num_occurences[index]);
-        print_hashtags(num_occurences[index]);
-        fprintf(stdout, "\n");
+            //Print the number of occurences in a right-aligned way with 4 characters
+            fprintf(stdout, "%c: %*d ", c, WIDTH, num_occurences[index]);
+
+            //Print the number of hashtags that corresponds to the number of occurences of the character
+            print_hashtags(num_occurences[index]);
+
+            //Newline
+            fprintf(stdout, "\n");
+        }
     }
-
+    
     return 0;
 }
