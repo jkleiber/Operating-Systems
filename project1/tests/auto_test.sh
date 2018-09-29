52fdec1e75d5894be26a0ca704d0490f1701d5b0
Justin Kleiber <jkleiber8@gmail.com>
2018-09-28 21:15:36 -0500

    adds more cleanup

--- project1/tests/auto_test.sh
+++ project1/tests/auto_test.sh
@@ -11,26 +11,32 @@ touch ~/Operating-Systems/project1/output/rstiles2_test_out.txt
 #Test 1
 /projects/1/project1 ~/Operating-Systems/project1/tests/awyatt.txt > ~/Operating-Systems/project1/output/awyatt_test_out.txt
 find . ! -name 'auto_test.sh' -type f -exec rm -f {} +
+sudo rm -R -- */
 
 #Test 2
 /projects/1/project1 ~/Operating-Systems/project1/tests/thanhd.txt > ~/Operating-Systems/project1/output/thanhd_test_out.txt
 find . ! -name 'auto_test.sh' -type f -exec rm -f {} +
+sudo rm -R -- */
 
 #Test 3
 /projects/1/project1 ~/Operating-Systems/project1/tests/gscott-1.txt > ~/Operating-Systems/project1/output/gscott_test_out.txt
 find . ! -name 'auto_test.sh' -type f -exec rm -f {} +
+sudo rm -R -- */
 
 #Test 4
 /projects/1/project1 ~/Operating-Systems/project1/tests/esc.txt > ~/Operating-Systems/project1/output/esc_test_out.txt
 find . ! -name 'auto_test.sh' -type f -exec rm -f {} +
+sudo rm -R -- */
 
 #Test 5
 /projects/1/project1 ~/Operating-Systems/project1/tests/rstiles1.txt > ~/Operating-Systems/project1/output/rstiles1_test_out.txt
 find . ! -name 'auto_test.sh' -type f -exec rm -f {} +
+sudo rm -R -- */
 
 #Test 6
 /projects/1/project1 ~/Operating-Systems/project1/tests/rstiles2.txt > ~/Operating-Systems/project1/output/rstiles2_test_out.txt
 find . ! -name 'auto_test.sh' -type f -exec rm -f {} +
+sudo rm -R -- */
 
 #diff files
 echo "Test 1";
