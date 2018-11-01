4caeaff3827694a4a8746419df90efbd85065435
Justin Kleiber <jkleiber8@gmail.com>
2018-11-01 15:04:50 -0500

    Adds a copy script and a readme

---
+++ project3/copy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp zinspect /projects/3/
+cp zformat /projects/3/
+cp zfilez /projects/3/
+cp zmkdir /projects/3/
+cp zrmdir /projects/3/
+cp *.c /projects/3/
+cp *.h /projects/3/
+cp README.txt /projects/3/
+cp Makefile /projects/3/
+
+#change permissions
+#chmod 777 /projects --recursive
+#chgrp google-sudoers /projects --recursive
