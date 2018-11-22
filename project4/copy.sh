806b277098b1cd2cfcbe96ab0f84e45d49064a2c
Justin Kleiber <jkleiber8@gmail.com>
2018-11-21 18:00:20 -0600

    updates copy function and adds bonus tar file from last week's submission

--- project4/copy.sh
+++ project4/copy.sh
@@ -5,6 +5,11 @@ cp zformat /projects/4/
 cp zfilez /projects/4/
 cp zmkdir /projects/4/
 cp zrmdir /projects/4/
+cp ztouch /projects/4/
+cp zcreate /projects/4/
+cp zappend /projects/4/
+cp zmore /projects/4/
+cp zremove /projects/4/
 cp *.c /projects/4/
 cp *.h /projects/4/
 cp README.txt /projects/4/
